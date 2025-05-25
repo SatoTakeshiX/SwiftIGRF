0b6d694676c625ebe4515f31a271c76e01f6059a
satoutakeshi <sato.takeshi1987@gmail.com>
2025-05-25 19:55:28 +0900

    IGRFBuilderWithGeocentricComponents

--- Sources/Client/IGRFClient.swift
+++ Sources/Client/IGRFClient.swift
@@ -22,45 +22,6 @@ public struct IGRFBuilder {
     }
 }
 
-public struct IGRFBuilderWithGeocentricComponents {
-    let igrfGen: IGRFGen
-    let coordinateSystem: CoordinateSystemType
-    let inputLocation: IGRFLocation
-    let degreesLocation: DegreesLocation
-    let components: GeocentricCoordinateComponents
-
-    public init(
-        igrfGen: IGRFGen,
-        coordinateSystem: CoordinateSystemType,
-        inputLocation: IGRFLocation,
-        degreesLocation: DegreesLocation,
-        components: GeocentricCoordinateComponents
-    ) {
-        self.igrfGen = igrfGen
-        self.coordinateSystem = coordinateSystem
-        self.inputLocation = inputLocation
-        self.degreesLocation = degreesLocation
-        self.components = components
-    }
-
-    public func set(date: Date) throws -> IGRFBuilderWithDate {
-        let dateDouble = date.decimalYear()
-
-        guard (1900...2035).contains(dateDouble) else {
-            throw IGRFError.invalidDate(
-                message: "Invalid date. Please enter a date between 1900 and 2035.")
-        }
-        return IGRFBuilderWithDate(
-            igrfGen: igrfGen,
-            coordinateSystem: coordinateSystem,
-            inputLocation: inputLocation,
-            degreesLocation: degreesLocation,
-            components: components,
-            date: dateDouble
-        )
-    }
-}
-
 public struct IGRFBuilderWithDate {
     let igrfGen: IGRFGen
     let coordinateSystem: CoordinateSystemType
